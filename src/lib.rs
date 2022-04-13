8536ae79efd39137626e3e1d8d2f64ac0a463181
Raúl Kripalani <raul@protocol.ai>
2022-04-13 14:17:43 +0200

    make structure clearer.

--- src/lib.rs
+++ src/lib.rs
@@ -8,12 +8,7 @@ use fvm_ipld_encoding::{to_vec, CborStore, RawBytes, DAG_CBOR};
 use fvm_sdk as sdk;
 use fvm_sdk::message::NO_DATA_BLOCK_ID;
 
-/// The state object.
-#[derive(Serialize_tuple, Deserialize_tuple, Clone, Debug, Default)]
-pub struct State {
-    pub count: u64,
-}
-
+/// A macro to abort concisely.
 macro_rules! abort {
     ($code:ident, $msg:literal $(, $ex:expr)*) => {
         fvm_sdk::vm::abort(
@@ -23,39 +18,56 @@ macro_rules! abort {
     };
 }
 
+/// The state object.
+#[derive(Serialize_tuple, Deserialize_tuple, Clone, Debug, Default)]
+pub struct State {
+    pub count: u64,
+}
+
+impl State {
+    pub fn load() -> Self {
+        // First, load the current state root.
+        let root = match sdk::sself::root() {
+            Ok(root) => root,
+            Err(err) => abort!(USR_ILLEGAL_STATE, "failed to get root: {:?}", err),
+        };
+
+        // Load the actor state from the state tree.
+        match Blockstore.get_cbor::<Self>(&root) {
+            Ok(Some(state)) => state,
+            Ok(None) => abort!(USR_ILLEGAL_STATE, "state does not exist"),
+            Err(err) => abort!(USR_ILLEGAL_STATE, "failed to get state: {}", err),
+        }
+    }
+
+    pub fn save(&self) -> Cid {
+        let serialized = match to_vec(self) {
+            Ok(s) => s,
+            Err(err) => abort!(USR_SERIALIZATION, "failed to serialize state: {:?}", err),
+        };
+        let cid = match sdk::ipld::put(Code::Blake2b256.into(), 32, DAG_CBOR, serialized.as_slice())
+        {
+            Ok(cid) => cid,
+            Err(err) => abort!(USR_SERIALIZATION, "failed to store initial state: {:}", err),
+        };
+        if let Err(err) = sdk::sself::set_root(&cid) {
+            abort!(USR_ILLEGAL_STATE, "failed to set root ciid: {:}", err);
+        }
+        cid
+    }
+}
+
 /// The actor's WASM entrypoint. It takes the ID of the parameters block,
 /// and returns the ID of the return value block, or NO_DATA_BLOCK_ID if no
 /// return value.
 #[no_mangle]
 pub fn invoke(_: u32) -> u32 {
-    match sdk::message::method_number() {
-        1 => {
-            // TODO a duplicate constructor invocation will reset state
-            //      this should check if the state already exists and fail if that's the case
-            constructor();
-            return NO_DATA_BLOCK_ID;
-        }
-        2 => {} // fallthrough
-        _ => {
-            abort!(USR_UNHANDLED_MESSAGE, "unrecognized method");
-        }
-    }
-
-    // First, load the current state root.
-    let root = match sdk::sself::root() {
-        Ok(root) => root,
-        Err(err) => abort!(USR_ILLEGAL_STATE, "failed to get root: {:?}", err),
-    };
-
-    // Load the actor state from the state tree.
-    let state = match Blockstore.get_cbor::<State>(&root) {
-        Ok(Some(state)) => state,
-        Ok(None) => abort!(USR_ILLEGAL_STATE, "state does not exist"),
-        Err(err) => abort!(USR_ILLEGAL_STATE, "failed to get state: {}", err),
-    };
-
     // Conduct method dispatch. Handle input parameters and return data.
-    let ret: Option<RawBytes> = say_hello(state);
+    let ret: Option<RawBytes> = match sdk::message::method_number() {
+        1 => constructor(),
+        2 => say_hello(),
+        _ => abort!(USR_UNHANDLED_MESSAGE, "unrecognized method"),
+    };
 
     // Insert the return data block if necessary, and return the correct
     // block ID.
@@ -70,32 +82,14 @@ pub fn invoke(_: u32) -> u32 {
 
 pub fn constructor() -> Option<RawBytes> {
     let state = State::default();
-    save_state(&state);
+    state.save();
     None
 }
 
-fn save_state(state: &State) -> Cid {
-    let serialized = match to_vec(&state) {
-        Ok(s) => s,
-        Err(err) => abort!(
-            USR_SERIALIZATION,
-            "failed to serialize initial state: {:?}",
-            err
-        ),
-    };
-    let cid = match sdk::ipld::put(Code::Blake2b256.into(), 32, DAG_CBOR, serialized.as_slice()) {
-        Ok(cid) => cid,
-        Err(err) => abort!(USR_SERIALIZATION, "failed to store initial state: {:}", err),
-    };
-    if let Err(err) = sdk::sself::set_root(&cid) {
-        abort!(USR_ILLEGAL_STATE, "failed to set root ciid: {:}", err);
-    }
-    cid
-}
-
-pub fn say_hello(mut state: State) -> Option<RawBytes> {
+pub fn say_hello() -> Option<RawBytes> {
+    let mut state = State::load();
     state.count += 1;
-    save_state(&state);
+    state.save();
 
     let ret = to_vec(format!("Hello world #{}!", &state.count).as_str());
     match ret {
